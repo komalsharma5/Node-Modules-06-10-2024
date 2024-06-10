9c8c4d5747c7536b9ef849eea047a5d33497c64c
Komal Sharma <sharmakomalweb@gmail.com>
2024-06-10 07:35:46 +0530

    Add files via upload

---
+++ export.js
@@ -0,0 +1,33 @@
+// const hello = () =>{
+//     console.log("Hello surat");
+// }
+
+// module.exports = hello
+
+const hello1 = () =>{
+    console.log("Hello surat1");
+}
+
+const hello2 = () =>{
+    console.log("Hello surat2");
+}
+
+const hello3 = () => {
+    console.log("Hello surat3");
+}
+
+const hello4 = () => {
+    console.log("Hello surat4");
+}
+
+const hello5 = () => {
+    console.log("Hello surat5");
+}
+
+module.exports = {
+    hello1,
+    hello2,
+    hello3,
+    hello4,
+    hello5
+}
---
+++ import.js
@@ -0,0 +1,16 @@
+// const hello = require("./export")
+
+// hello()
+
+const hello = require("./export")
+
+hello.hello1()
+hello.hello2()
+hello.hello3()
+hello.hello4()
+hello.hello5()
+
+
+// const os = require('os')
+
+// console.log(os.cpus().length);
